77d79a9920528a11a40ec65e81d4c54e27ddea69
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2022-08-16 09:53:47 -0300

    Add a function that plays a single round of rock paper scissors

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function getRobotChoice() {
     var choice = choicesList[Math.floor(Math.random() * 3)];
     return choice;
 }
+
+// Plays a round, the Computer choice is optional, making the default a random result.
+function playRound(playerChoice, robotChoice=getRobotChoice()){
+    playerChoice = playerChoice.toLowerCase();
+    robotChoice = robotChoice.toLowerCase();
+
+    const resultMsg = `Player |${playerChoice}| vs Robot |${robotChoice}|`;
+
+    if (playerChoice === robotChoice){
+        return resultMsg + '\nTie!';
+    }else if (
+        (playerChoice === "rock" && robotChoice === "scissors") ||
+        (playerChoice === "scissors" && robotChoice === "paper") ||
+        (playerChoice === "paper" && robotChoice === "rock")){
+        return resultMsg + '\nYou Win!';
+    }else{
+        return resultMsg + '\nYou Lose!';
+    }
+}
