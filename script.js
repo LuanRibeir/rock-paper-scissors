8b86d7e52b7aad4a9815c1c0c4a0f91e3ea464b3
Luan Ribeiro <luaneribeiroalmeida@gmail.com>
2022-10-05 14:42:25 -0300

    Format script.js

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 // Returns a randomly result of either rock paper scissors.
 function getRobotChoice() {
-    var choicesList = ["rock","paper","scissors"];
-    var choice = choicesList[Math.floor(Math.random() * 3)];
-    return choice;
+	var choicesList = ['rock', 'paper', 'scissors'];
+	var choice = choicesList[Math.floor(Math.random() * 3)];
+	return choice;
 }
 
 // Plays a round, the Computer choice is optional, making the default a random result.
-function playRound(playerChoice, robotChoice=getRobotChoice()){
-    playerChoice = playerChoice.toLowerCase();
-    robotChoice = robotChoice.toLowerCase();
+function playRound(playerChoice, robotChoice = getRobotChoice()) {
+	playerChoice = playerChoice.toLowerCase();
+	robotChoice = robotChoice.toLowerCase();
 
-    const resultMsg = `Player |${playerChoice}| vs Robot |${robotChoice}|`;
+	const resultMsg = `Player |${playerChoice}| vs Robot |${robotChoice}|`;
 
-    if (playerChoice === robotChoice){
-        return resultMsg + '\nTie!';
-    }else if (
-        (playerChoice === "rock" && robotChoice === "scissors") ||
-        (playerChoice === "scissors" && robotChoice === "paper") ||
-        (playerChoice === "paper" && robotChoice === "rock")){
-        return resultMsg + '\nYou Win!';
-    }else{
-        return resultMsg + '\nYou Lose!';
-    }
+	if (playerChoice === robotChoice) {
+		return resultMsg + '\nTie!';
+	} else if (
+		(playerChoice === 'rock' && robotChoice === 'scissors') ||
+		(playerChoice === 'scissors' && robotChoice === 'paper') ||
+		(playerChoice === 'paper' && robotChoice === 'rock')
+	) {
+		return resultMsg + '\nYou Win!';
+	} else {
+		return resultMsg + '\nYou Lose!';
+	}
 }
 
 // Plays a 5 round game.
-function game(){
-    for (let i = 1; i<=5; i++) {
-        alert(playRound(prompt())); 
-    }
+function game() {
+	for (let i = 1; i <= 5; i++) {
+		alert(playRound(prompt()));
+	}
 }
 
 game();
